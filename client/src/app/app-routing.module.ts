3aa2661646339e661cc5400a5597b817a508f265
Milind Yadav <yadavmilind08@gmail.com>
2021-06-23 16:50:47 +0530

    Created member edit component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
@@ -16,6 +17,7 @@ const routes: Routes = [
     path: '', runGuardsAndResolvers: 'always', canActivate: [AuthGuard], children: [
       { path: 'members', component: MemberListComponent, canActivate: [AuthGuard] },
       { path: 'members/:username', component: MemberDetailComponent },
+      { path: 'member/edit', component: MemberEditComponent },
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
     ]
