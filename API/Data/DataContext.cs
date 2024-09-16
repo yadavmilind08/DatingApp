288b22cbd1f6905ca30a3e54d0431bb676cf8b0b
Milind Yadav <yadavmilind08@gmail.com>
2024-09-16 23:01:45 +0530

    Created project

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+
+namespace API.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
