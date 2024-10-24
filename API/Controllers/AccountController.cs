a1bd4204313e45245c22da7b6f272d5302818785
Milind Yadav <yadavmilind08@gmail.com>
2024-10-24 19:28:57 +0530

    Added login dto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -29,6 +29,24 @@ namespace API.Controllers
             return user;
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+          var user = await context.Users.FirstOrDefaultAsync(x=> 
+            x.UserName == loginDto.Username.ToLower()); 
+
+          if(user == null) return Unauthorized("Invalid username");  
+
+          using var hmac = new HMACSHA512(user.PasswordSalt);  
+          var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+          for(int i=0; i<computedHash.Length; i++){
+            if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+          }
+
+          return user;
+        }
+
         private async Task<bool> UserExits(string username)
         {
             return await context.Users.AnyAsync(x=> x.UserName.ToLower()== username.ToLower());
