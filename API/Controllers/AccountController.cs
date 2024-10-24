30a615465e276e0f0d89ca5a4f6f5b461cfad21f
Milind Yadav <yadavmilind08@gmail.com>
2024-10-24 19:14:59 +0530

    Added user exists method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,21 +1,25 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class AccountController(DataContext context): BaseApiController
     {
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password){
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if(await UserExits(registerDto.Username)) return BadRequest("Username is taken");
             using var hmac = new HMACSHA512();
 
             var user = new AppUser 
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -23,6 +27,11 @@ namespace API.Controllers
             await context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool> UserExits(string username)
+        {
+            return await context.Users.AnyAsync(x=> x.UserName.ToLower()== username.ToLower());
         }
     }
 }
