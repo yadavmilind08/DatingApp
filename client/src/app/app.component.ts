2737ddfe85d7d3512196234faad75c5e6524fe55
Milind Yadav <yadavmilind08@gmail.com>
2024-10-29 22:40:25 +0530

    input output communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
@@ -13,13 +12,10 @@ import { HomeComponent } from './home/home.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
   private accountService = inject(AccountService);
   title = 'DatingApp';
-  users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -28,13 +24,5 @@ export class AppComponent implements OnInit {
     if (!userString) return;
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response: any) => (this.users = response),
-      error: (error: any) => console.log(error),
-      complete: () => console.log('Request has completed'),
-    });
   }
 }
