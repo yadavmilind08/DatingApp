68da57eede37ec0b1cc276bdce080031bb95c585
Milind Yadav <yadavmilind08@gmail.com>
2024-11-06 18:17:43 +0530

    Included auto mapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,27 +1,26 @@
-using API.Data;
-using API.Entities;
+using API.DTOs;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        var users = await userRepository.GetUsersAsync();
+        var users = await userRepository.GetMembersAsync();
 
         return Ok(users);
     }
 
     [HttpGet("{username}")]
-    public async Task<ActionResult<AppUser>> GetUser(string username)
+    public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        var user = await userRepository.GetUserByUsernameAsync(username);
+        var user = await userRepository.GetMemberAsync(username);
 
         if (user == null) return NotFound();
 
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -21,9 +21,9 @@ namespace API.Entities
         public required string Country { get; set; }
         public List<Photo> Photos { get; set; } = [];
 
-        public int GetAge()
-        {
-            return DateOfBirth.CalculateAge();
-        }
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
     }
 }
